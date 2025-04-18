673780b8cf4079b725a38fc14f467db4c778b740
Ubuntu <ubuntu@ip-172-31-34-144.ap-south-1.compute.internal>
2025-04-18 17:52:46 +0000

    Cloud 2

--- src/services/api.js
+++ src/services/api.js
@@ -34,7 +34,7 @@ const debugAuth = () => {
 // Create axios instance with default config
 const api = axios.create({
 
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://13.232.180.68:5000/api",
   headers: {
     'Content-Type': 'application/json'
   }
